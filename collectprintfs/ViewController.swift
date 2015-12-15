dda3f8cf0c3675fe9ab364678c603e5c298e8268
Thomas <oot@thomass-mbp>
2015-12-15 21:27:23 +0100

    Initial Commit

---
+++ collectprintfs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  collectprintfs
+//
+//  Created by Thomas on 15.12.15.
+//  Copyright © 2015 thgr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
